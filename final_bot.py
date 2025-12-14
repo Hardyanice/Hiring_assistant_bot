9d0b0594dab1f25e5ca03f90005e32f2c7b4f746
Souhardya Das <dassouhardya1720@gmail.com>
2025-12-14 12:39:27 +0530

    Update final_bot.py

--- final_bot.py
+++ final_bot.py
@@ -218,7 +218,7 @@ def bot_reply(user_message):
 
             # Compute max similarity
             sims = [cosine_similarity(user_vec, g) for g in greet_vecs]
-            return max(sims) > 0.50   # threshold tuned
+            return max(sims) > 0.30
         except:
             return False
 
@@ -391,6 +391,7 @@ if user_input:
     st.session_state.chat_history.append(("Assistant", bot_message))
     st.session_state["force_rerun"] = True
     st.rerun()
+
 
 
 
